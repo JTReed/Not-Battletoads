65ed4a3b912916fe95ccd515ef4f4a0170aed336
Nik Ingrassia <ningrassia@wisc.edu>
2014-04-04 20:58:50 -0400

    Updated GameCamera script - now follows player X position.

--- Assets/Scripts/GameCamera.cs
+++ Assets/Scripts/GameCamera.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class GameCamera : MonoBehaviour {
 
+	public GameObject player;
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	
+	//do cam movement late!
+	void LateUpdate(){
+		transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
 	}
 }
