8035eb84df9641b3dff982fd19fc8d55efb945cb
JTReed <reed.jacksont@gmail.com>
2014-04-05 06:59:49 -0500

    ANIMATION STATION, BABY

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,21 +10,30 @@ public class PlayerController : MonoBehaviour {
 
     private Vector3 currentSpeed;
     private Vector3 targetSpeed;
+    private Vector3 fwdVector;
     private float punchStartTime;
-    private float punchDuration = .5f;
+    private float punchDuration = .4f;
 
     private bool jump;
     private bool punching;
 
+    private LayerMask attackLayerMask;
+
     private Transform spriteTransform;
     private CharacterController charControl;
     private Animator animator;
+
+    void Awake()
+    {
+        attackLayerMask = LayerMask.NameToLayer("Enemy") << 1;
+    }
 
 	// Use this for initialization
 	void Start () {
         charControl = GetComponent<CharacterController>();
         spriteTransform = transform.FindChild("Sprite");
         animator = spriteTransform.GetComponent<Animator>();
+        fwdVector = Vector3.right;
         currentSpeed = Vector3.zero;
 	}
 	
@@ -33,8 +42,10 @@ public class PlayerController : MonoBehaviour {
 
         //handle jump
         if (charControl.isGrounded) {
+            animator.SetBool("Jumping", false);
             currentSpeed.y = 0;
             if (Input.GetButtonDown("Jump")) {
+                animator.SetBool("Jumping", true);
                 currentSpeed.y += jumpSpeed;
             }
         }
@@ -47,10 +58,43 @@ public class PlayerController : MonoBehaviour {
         currentSpeed.x = MoveToward(currentSpeed.x, targetSpeed.x, acceleration);
         currentSpeed.z = MoveToward(currentSpeed.z, targetSpeed.z, acceleration);
 
+        // handle punching
+        if (!punching) {
+            if (Input.GetButtonDown("Punch")) {
+
+                punching = true;
+                currentSpeed = Vector3.zero;
+                animator.SetBool("Punching", true);
+                punchStartTime = Time.time;
+            }
+        }
+        else {
+            if (Time.time - punchStartTime >= punchDuration) {
+                punching = false;
+                animator.SetBool("Punching", false);
+            }
+            else {
+                currentSpeed = Vector3.zero;
+                RaycastHit hit;
+                Debug.DrawRay(transform.position + new Vector3(0, transform.position.y / 2.0f, 0), fwdVector * 1.5f, Color.red);
+                if (Physics.Raycast(transform.position + new Vector3(0, transform.position.y / 2.0f, 0), fwdVector, out hit, 1.5f, attackLayerMask)) {
+                    Debug.Log("punched enemy yeah!!");
+                }
+            }
+        }
+
+        if (Mathf.Abs(currentSpeed.x) > 0 || Mathf.Abs(currentSpeed.z) > 0) {
+            animator.SetFloat("Speed", 1);
+        }
+        else {
+            animator.SetFloat("Speed", 0);
+        }
+
         float facing = Mathf.Sign(Input.GetAxisRaw("Horizontal"));
         if (targetSpeed.x != 0) {
             // flip character sprite if going left
             spriteTransform.eulerAngles = (facing < 0) ? (Vector3.up * 180) + (Vector3.right * 315) : Vector3.right * 45;
+            fwdVector = (facing < 0) ? Vector3.left : Vector3.right;
         }
 
 
@@ -59,6 +103,7 @@ public class PlayerController : MonoBehaviour {
         // handle punching
         if (!punching) {
             if (Input.GetButtonDown("Punch")) {
+
                 punching = true;
                 animator.SetBool("Punching", true);
                 punchStartTime = Time.time;
@@ -68,6 +113,13 @@ public class PlayerController : MonoBehaviour {
             if (Time.time - punchStartTime >= punchDuration) {
                 punching = false;
                 animator.SetBool("Punching", false);
+            }
+            else {
+                RaycastHit hit;
+                Debug.DrawRay(transform.position + new Vector3(0, transform.position.y / 2.0f, 0), fwdVector * 1.5f, Color.red);
+                if (Physics.Raycast(transform.position + new Vector3(0, transform.position.y / 2.0f, 0), fwdVector, out hit, 1.5f, attackLayerMask)) {
+                    Debug.Log("punched enemy yeah!!");
+                }
             }
         }
 	}
