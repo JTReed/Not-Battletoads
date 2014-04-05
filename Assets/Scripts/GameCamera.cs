3cf80244c7dabe6839c997f555d81884c154f4d1
Nik Ingrassia <ningrassia@wisc.edu>
2014-04-05 08:32:50 -0500

    Added camera lock to level. minX/maxX on GameCamera script.

--- Assets/Scripts/GameCamera.cs
+++ Assets/Scripts/GameCamera.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class GameCamera : MonoBehaviour {
 
 	private Transform target;
+    public float minX;
+    public float maxX;
 
 	// Use this for initialization
 	void Start () {
@@ -18,8 +20,13 @@ public class GameCamera : MonoBehaviour {
 	//do cam movement late!
 	void LateUpdate(){
         if (target != null) {
-            //set camera for player's X position, keep previous y/z position.
-            transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+            //set camera for player's X position, keep previous y/z position. (with min/max X position)
+            if (target.position.x < minX)
+                transform.position = new Vector3(minX, transform.position.y, transform.position.z);
+            else if (target.position.x > maxX)
+                transform.position = new Vector3(maxX, transform.position.y, transform.position.z);
+            else
+                transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
         }
 	}
 
