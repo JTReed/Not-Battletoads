40674b644a767c49d9b20d7d43b6e24cf8e0eb87
JTReed <reed.jacksont@gmail.com>
2014-04-04 19:35:14 -0500

    Set up base WorldPolygon.prefab and script

---
+++ Assets/Scripts/WorldPolygon.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldPolygon : MonoBehaviour {
+
+    public bool walkable;
+    public Vector2 leftRightVec;
+    public Vector2 upDownVec;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
