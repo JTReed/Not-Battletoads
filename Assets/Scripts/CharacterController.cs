8a9a61980804396d3756a75224687ab4f2304537
JTReed <reed.jacksont@gmail.com>
2014-04-04 20:19:28 -0500

    - Added basic CHaracter movement

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -2,14 +2,39 @@
 using System.Collections;
 
 public class CharacterController : MonoBehaviour {
+    public float speed;
+    public float acceleration;
+
+    private Vector2 currentSpeed;
+    private Vector2 targetSpeed;
+
+    // private floor collider?
 
 	// Use this for initialization
 	void Start () {
-	
+        currentSpeed = Vector2.zero;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        Vector2 targetSpeed = new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed);
+        currentSpeed.x = MoveToward(currentSpeed.x, targetSpeed.x, acceleration);
+        currentSpeed.y = MoveToward(currentSpeed.y, targetSpeed.y, acceleration);
+
+        transform.Translate(currentSpeed);
 	}
+
+    float MoveToward(float curr, float targ, float accel)
+    {
+        if (curr == targ) {
+            return curr;
+        }
+
+        float direction = Mathf.Sign(targ - curr);
+        curr += accel * direction * Time.deltaTime;
+        // return the target if curr has passed it
+        return (Mathf.Sign(curr - targ) == direction) ? curr : targ;
+    }
+
 }
--- Assets/Scripts/WorldPolygon.cs
+++ Assets/Scripts/WorldPolygon.cs
@@ -14,6 +14,6 @@ public class WorldPolygon : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
 }
