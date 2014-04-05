fdcb109b96ba775556d35382973f8ad4855f57a0
Nik Ingrassia <ningrassia@wisc.edu>
2014-04-05 09:05:16 -0500

    Added lock to GameCamera (locked)

--- Assets/Scripts/GameCamera.cs
+++ Assets/Scripts/GameCamera.cs
@@ -6,10 +6,10 @@ public class GameCamera : MonoBehaviour {
 	private Transform target;
     public float minX;
     public float maxX;
-
+    public bool locked;
 	// Use this for initialization
 	void Start () {
-		
+        locked = false;
 	}
 	
 	// Update is called once per frame
@@ -20,13 +20,16 @@ public class GameCamera : MonoBehaviour {
 	//do cam movement late!
 	void LateUpdate(){
         if (target != null) {
-            //set camera for player's X position, keep previous y/z position. (with min/max X position)
-            if (target.position.x < minX)
-                transform.position = new Vector3(minX, transform.position.y, transform.position.z);
-            else if (target.position.x > maxX)
-                transform.position = new Vector3(maxX, transform.position.y, transform.position.z);
-            else
-                transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+            if (!locked)
+            {
+                //set camera for player's X position, keep previous y/z position. (with min/max X position)
+                if (target.position.x < minX)
+                    transform.position = new Vector3(minX, transform.position.y, transform.position.z);
+                else if (target.position.x > maxX)
+                    transform.position = new Vector3(maxX, transform.position.y, transform.position.z);
+                else
+                    transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+            }
         }
 	}
 
